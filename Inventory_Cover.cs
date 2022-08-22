79a31a5f8ef60812f852af14b50c713b4b564a0e
Darnae <108689727+Darnae@users.noreply.github.com>
2022-08-21 23:09:35 -0400

    Add files via upload

---
+++ Inventory_Cover.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Updated_Inventory
+{
+    public partial class Inventory_Cover : Form
+    {
+        public Inventory_Cover()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Button_Click(object sender, EventArgs e)
+        {
+            Inventory update = new Inventory();
+
+            update.Show();
+        }
+    }
+}
